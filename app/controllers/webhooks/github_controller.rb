7d3787f438412b3d8111ecb23e5c05207fb60f9c
june29 <june29.jp@gmail.com>
2018-08-08 00:01:40 +0900

    :muscle: Validate request

--- app/controllers/webhooks/github_controller.rb
+++ app/controllers/webhooks/github_controller.rb
@@ -2,6 +2,19 @@ class Webhooks::GithubController < ApplicationController
   protect_from_forgery except: :create
 
   def create
+    head 400 and return unless valid_request?
+
     render json: { message: 'dummy' }
+  end
+
+  def valid_request?
+    their_signature_header = request.env['HTTP_X_HUB_SIGNATURE'] || 'sha1='
+    method, their_digest = their_signature_header.split('=')
+
+    our_digest = OpenSSL::HMAC.hexdigest(method, ENV['GITHUB_WEBHOOK_SECRET'], request.raw_post)
+
+    their_digest == our_digest
+  rescue => error
+    false
   end
 end
