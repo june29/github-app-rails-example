e1a5c6ddad718434ba32a2aa2ca7a480f1e9f10e
june29 <june29.jp@gmail.com>
2018-08-07 23:37:14 +0900

    :wrench: Configure dotenv

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Railtie.load
+end
+
 module GithubAppExample
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
